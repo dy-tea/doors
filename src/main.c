5248e1a64422944a80ee2d4527b6777e0323ad3c
Dylan Donnell <dylan.donnell@student.griffith.ie>
2026-06-27 20:17:56 +0200

    feat: set SCHED_RR before server_run

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "config.h"
 #include "log.h"
 #include "server.h"
+#include <sched.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 #include <wlr/util/log.h>
 
 struct server_t server = {0};
@@ -10,6 +12,22 @@ struct server_t server = {0};
 static void usage(const char *argv0) {
   printf("Usage: %s [-c <config-dir>]\n", argv0);
   printf("  -c, --config <dir>  Use specified config directory instead of default\n");
+}
+
+static void child_fork_callback(void) {
+  struct sched_param param = {.sched_priority = 0};
+  pthread_setschedparam(pthread_self(), SCHED_OTHER, &param);
+}
+
+static void set_rr_scheduling(void) {
+  int prio = sched_get_priority_min(SCHED_RR);
+  struct sched_param param = {.sched_priority = prio};
+  if (pthread_setschedparam(pthread_self(), SCHED_RR, &param) != 0) {
+    wlr_log(WLR_INFO, "Failed to set real-time scheduling to %d", prio);
+    return;
+  }
+  pthread_atfork(NULL, NULL, child_fork_callback);
+  wlr_log(WLR_DEBUG, "Real-time scheduling enabled (SCHED_RR, priority=%d)", prio);
 }
 
 int main(int argc, char *argv[]) {
@@ -45,6 +63,7 @@ int main(int argc, char *argv[]) {
 
   config_init_with_config_dir(config_dir);
   server_init();
+  set_rr_scheduling();
   int ret = server_run();
   server_fini();
   log_fini();
